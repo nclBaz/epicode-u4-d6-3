14a01454437f1e6536b865ae3973f056280e2523
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-01-09 09:54:30 +0100

    Adds basic server stuff

---
+++ src/api/products/index.js
@@ -0,0 +1,41 @@
+import express from "express"
+
+const productsRouter = express.Router()
+
+productsRouter.post("/", async (req, res, next) => {
+  try {
+    throw new Error("KABOOOOOOOOOOOOOOOOOOOOM!")
+  } catch (error) {
+    next(error)
+  }
+})
+
+productsRouter.get("/", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+productsRouter.get("/:productId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+productsRouter.put("/:productId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+productsRouter.delete("/:productId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default productsRouter
---
+++ src/errorHandlers.js
@@ -0,0 +1,22 @@
+export const badRequestHandler = (err, req, res, next) => {
+  if (err.status === 400) {
+    res.status(400).send({ success: false, message: err.message })
+  } else {
+    next(err)
+  }
+} // 400
+
+export const notFoundHandler = (err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send({ success: false, message: err.message })
+  } else {
+    next(err)
+  }
+} // 404
+
+export const genericErrorHandler = (err, req, res, next) => {
+  console.log(err)
+  res
+    .status(500)
+    .send({ success: false, message: "An error occurred on our side! we gonna fix that asap!" })
+} // 500
